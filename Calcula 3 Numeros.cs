5e07e2f409288f6b35ebed629de4e9b9a1406f2a
Marlon Castillo <marloncastillo424@gmial.com>
2024-02-26 15:47:39 -0600

    Subiendo Tarea No.3

---
+++ Calcula 3 Numeros.cs
@@ -0,0 +1,29 @@
+﻿try
+{
+    Console.WriteLine("Este Programa calculara el número mayor de 3 que el usuario ingrese");
+    Console.WriteLine("\nIngrese el primer número:");
+    int num1 = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Ingrese el segundo número:");
+    int num2 = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Ingrese el tercer número:");
+    int num3 = int.Parse(Console.ReadLine());
+
+    if (num1 > num2)
+    {
+        Console.WriteLine("El número Mayor ingresado es: " + num1);
+    }
+    else if (num1 < num2 && num3 < num2)
+    {
+        Console.WriteLine("El número Mayor ingresado es: " + num2);
+    }
+    else
+    {
+        Console.WriteLine("El número Mayor ingresado es: " + num3);
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Usted no Introdujo un Numero Valido");
+}
